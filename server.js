dcbc3cde6cc9ea8dc994708acee7543c7331edf9
Brian LeRoux <brian@westcoastlogic.com>
2010-06-07 22:22:41 -0500

    big fuck around reorg

--- server.js
+++ server.js
@@ -1,19 +1,2 @@
-require.paths.unshift("lib/express/lib");
-require.paths.unshift("lib/wtfjs");
-require.paths.unshift("lib/showdown-v0.9/src");
-require.paths.unshift("lib/prettyfy");
-
-require("express");
-require("express/plugins");
-require("wtf");
-
-// config block; sorta fugly
-configure(function() {
-    set('root', __dirname + '/lib/wtfjs');
-    // set('views', __dirname + '/views');
-    use(Logger);
-    use(Static);
-    enable('show exceptions');
-});
-
-run(parseInt(process.env.PORT || 8000), null);
+require.paths.unshift("lib/codeblog");
+require("codeblog");
