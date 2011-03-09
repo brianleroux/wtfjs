73d0432075295060b4c38b6053a1d9265b90f55e
brianleroux <brian@westcoastlogic.com>
2011-03-08 18:24:34 -0800

    whoops. make the feed sort proper like

--- test/run.js
+++ test/run.js
@@ -6,6 +6,10 @@ var testrunner = require('nodeunit').testrunner
 exports['tests post'] = function(test){
     test.ok(Post, 'post exists')
     test.equals(Post.page(1).posts.length, 5, '5 post per page')
+
+    var page1 = Post.page(1).posts
+
+
     test.done()
 }
 
