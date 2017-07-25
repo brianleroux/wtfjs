2e0ddd6e64963699f1ad0606cbefe9e547484430
Brian LeRoux <b@brian.io>
2017-07-24 17:42:24 -0700

    adds listing to index

--- src/html/get-index/index.js
+++ src/html/get-index/index.js
@@ -2,11 +2,20 @@ var fs = require('fs')
 var path = require('path')
 var arc = require('@architect/functions')
 var layout = require('@wtfjs/theme')
+var filter = f=> f.includes('.md')
+var files = fs.readdirSync(__dirname + '/node_modules/@wtfjs/md').filter(filter).reverse()
 var index = false
+
+function link(file) {
+  var val = file.replace('.md', '')
+  var name = val.replace(/-/g, ' ')
+  return `<li><a href=/wtfs/${val}>${name}</a></li>`
+}
 
 function route(req, res) {
   if (!index) index = fs.readFileSync(path.join(__dirname, 'index.html')).toString()
-  res({html:layout(index)})
+  var list = files.map(link).join('')
+  res({html:layout(index + list)})
 }
 
 exports.handler = arc.html.get(route)
