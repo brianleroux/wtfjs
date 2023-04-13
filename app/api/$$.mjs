bfc7e3af587cd7662d24fa59d865527870fd7db9
Sebastian Hädrich <sebastian.haedrich@mailbox.org>
2023-04-13 14:18:26 +0200

    remove unused lines of code

--- app/api/$$.mjs
+++ app/api/$$.mjs
@@ -1,15 +1,11 @@
-import { readFileSync, readdirSync } from 'fs'
+import { readdirSync } from 'fs'
 import { URL } from 'url'
 
-export async function get(req) {
-    // Get requested path
-    const { path: activePath } = req
-    let docPath = activePath || 'index'
-    
+export async function get() {    
     // Read markdown file
     const docsURL = new URL(`../md`, import.meta.url)
     const docs = readdirSync(docsURL.pathname, 'utf-8')
-    
+
     return {
         json: {
             docs: docs.map(doc => {
